cef4f6429c663e9c74b5ff9fbf9701df17ed4a11
jiin <amesystand@gmail.com>
2025-11-30 13:56:23 +0900

    upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,9 +10,22 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+=======
+import { BrowserRouter } from 'react-router-dom';
+import './font/Paperlogy.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+>>>>>>> ba48d3279e5f388cf91ffc77c43b2a345ab24891
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+<<<<<<< HEAD
 reportWebVitals();
+=======
+>>>>>>> ba48d3279e5f388cf91ffc77c43b2a345ab24891
